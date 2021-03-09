c84abb92257bf6ca982ad77b439d1a5930910ac3
Hamza Kamal <mail.hamzakam@gmail.com>
2021-03-09 12:59:13 +0530

    2.5 Seperate Module. Refactored Course into it's own js file.

--- part2/course-info/src/index.js
+++ part2/course-info/src/index.js
@@ -1,51 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const Header = ({ text }) => {
-    return <h2>{text}</h2>;
-};
-const Part = ({ part, exercises, id }) => {
-    return (
-        <p key={id}>
-            {part} {exercises}
-        </p>
-    );
-};
-const Content = ({ parts }) => {
-    return (
-        <div>
-            {parts.map((part) => {
-                return (
-                    <Part
-                        part={part.name}
-                        exercises={part.exercises}
-                        key={part.id}
-                    />
-                );
-            })}
-        </div>
-    );
-};
-const Total = ({ parts }) => {
-    return (
-        <b>
-            Total of{" "}
-            {parts.reduce((sum, part) => {
-                return sum + part.exercises;
-            }, 0)}{" "}
-            exercises.
-        </b>
-    );
-};
-const Course = ({ course }) => {
-    return (
-        <div>
-            <Header text={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-        </div>
-    );
-};
+import Course from "./Components/Course";
 const App = () => {
     const courses = [
         {
