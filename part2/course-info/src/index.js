943556a71600e82abba06a63fc9e16096b1f39bc
Hamza Kamal <mail.hamzakam@gmail.com>
2021-03-09 12:06:54 +0530

    2.1 CourseInfo. Modified app component by adding a course component.

---
+++ part2/course-info/src/index.js
@@ -0,0 +1,62 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const Header = ({ text }) => {
+    return <h1>{text}</h1>;
+};
+const Part = ({ part, exercises, id }) => {
+    return (
+        <p key={id}>
+            {part} {exercises}
+        </p>
+    );
+};
+const Content = ({ parts }) => {
+    return (
+        <div>
+            {parts.map((part) => {
+                return (
+                    <Part
+                        part={part.name}
+                        exercises={part.exercises}
+                        key={part.id}
+                    />
+                );
+            })}
+        </div>
+    );
+};
+const Course = ({ course }) => {
+    return (
+        <div>
+            <Header text={course.name} />
+            <Content parts={course.parts} />
+        </div>
+    );
+};
+const App = () => {
+    const course = {
+        id: 1,
+        name: "Half Stack application development",
+        parts: [
+            {
+                name: "Fundamentals of React",
+                exercises: 10,
+                id: 1,
+            },
+            {
+                name: "Using props to pass data",
+                exercises: 7,
+                id: 2,
+            },
+            {
+                name: "State of a component",
+                exercises: 14,
+                id: 3,
+            },
+        ],
+    };
+
+    return <Course course={course} />;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
