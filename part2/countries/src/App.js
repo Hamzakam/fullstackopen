5d6b204393cc1dbed1d29d9f8ba870e2b7d7cf1f
Hamza Kamal <mail.hamzakam@gmail.com>
2021-03-10 15:13:02 +0530

    2.13 Countries. Refactored Countries,Country and Languages Component into their own modules from App.js .

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,45 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-const Country = ({ country }) => {
-    return (
-        <div>
-            <h1>{country.name}</h1>
-            <p>Capital {country.capital}</p>
-            <p>Population {country.population}</p>
-            <h3>languages</h3>
-            <ul>
-                {country.languages.map((language) => {
-                    return <li key={language.name}>{language.name}</li>;
-                })}
-            </ul>
-            <img
-                width="200"
-                height="150"
-                src={country.flag}
-                alt={country.name + "flag"}
-            />
-        </div>
-    );
-};
-const Countries = ({ countries, newFilter }) => {
-    const filteredData = countries.filter((country) => {
-        return (
-            newFilter === "" ||
-            country.name.toLowerCase().includes(newFilter.toLowerCase())
-        );
-    });
-    if (filteredData.length <= 10) {
-        if (filteredData.length === 1) {
-            return <Country country={filteredData[0]} />;
-        } else {
-            return filteredData.map((country) => {
-                return <div key={country.name}>{country.name}</div>;
-            });
-        }
-    } else {
-        return <div>Too Many Countries.Specify another filter</div>;
-    }
-};
+import Countries from "./Components/Countries";
+
 const App = () => {
     const [newFilter, setNewFilter] = useState("");
     const [countries, setCountries] = useState([]);
---
+++ part2/countries/src/Components/Country.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Languages from "./Languages";
+
+const Country = ({ country, singleShow }) => {
+    const [showCountry, setShowCountry] = useState(false);
+    const handleShowClick = () => setShowCountry(!showCountry);
+
+    if (showCountry || singleShow === true) {
+        return (
+            <div>
+                <h1>{country.name}</h1>
+                <p>Capital {country.capital}</p>
+                <p>Population {country.population}</p>
+                <Languages languages={country.languages} />
+                <img
+                    width="200"
+                    height="150"
+                    src={country.flag}
+                    alt={country.name + "flag"}
+                />
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                {country.name}{" "}
+                <button onClick={handleShowClick}>{"show"}</button>
+            </div>
+        );
+    }
+};
+export default Country;
