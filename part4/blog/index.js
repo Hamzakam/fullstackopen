832c9d1a793ff39afacb5a28c9bc7af48c7b0026
Hamza Kamal <mail.hamzakam@gmail.com>
2021-03-14 17:26:36 +0530

    Refactored Code and Improved File Structure

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,42 +1,23 @@
-const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
-require("dotenv").config();
+const http = require("http");
+const app = require("./app");
+const { MONGODB_URI, PORT } = require("./utils/config");
 
-const app = express();
-
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number,
-});
-const Blog = mongoose.model("Blog", blogSchema);
-
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-});
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-    Blog.find({}).then((blogs) => {
-        response.json(blogs);
+mongoose
+    .connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+    })
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+        console.log("Failed to connect to MongoDB", error);
     });
-});
 
-app.post("/api/blogs", (request, response) => {
-    const blog = new Blog(request.body);
-
-    blog.save().then((result) => {
-        response.status(201).json(result);
-    });
-});
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+const server = http.createServer(app);
+server.listen(PORT, () => {
+    console.log(`Connected on PORT ${PORT}`);
 });
