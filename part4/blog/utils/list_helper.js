27c97594ec6f3d46f01f52a09a8beb8f7677d21f
Hamza Kamal <mail.hamzakam@gmail.com>
2021-03-15 10:55:54 +0530

    4.6 using lodash

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,3 +1,4 @@
+const _ = require("lodash");
 const dummy = (blogs) => {
     // console.log(blogs);
     return 1;
@@ -13,18 +14,31 @@ const favoriteBlog = (blogs) => {
         : {};
 };
 
+//most blogs without lodash
+// const mostBlogs = (blogs) => {
+//     let max = {};
+//     blogs.reduce((hashObject, blog) => {
+//         hashObject[blog.author] =
+//             hashObject[blog.author] === undefined
+//                 ? 1
+//                 : hashObject[blog.author] + 1;
+//         if (max.author === undefined || hashObject[blog.author] > max.blogs) {
+//             max = { author: blog.author, blogs: hashObject[blog.author] };
+//         }
+//         return hashObject;
+//     }, {});
+//     return max;
+// };
+
+//most blogs with lodash
 const mostBlogs = (blogs) => {
-    let max = {};
-    blogs.reduce((hashObject, blog) => {
-        hashObject[blog.author] =
-            hashObject[blog.author] === undefined
-                ? 1
-                : hashObject[blog.author] + 1;
-        if (max.author === undefined || hashObject[blog.author] > max.blogs) {
-            max = { author: blog.author, blogs: hashObject[blog.author] };
-        }
-        return hashObject;
-    }, {});
-    return max;
+    if (blogs.length > 0) {
+        const maxAuthorObj = _.maxBy(
+            Object.entries(_.groupBy(blogs, (blog) => blog.author)),
+            (ele) => ele[1].length
+        );
+        return { author: maxAuthorObj[0], blogs: maxAuthorObj[1].length };
+    }
+    return {};
 };
 module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs };
